2286aeb91e820a4290205316411d22d874a6dd90
deedy5 <65482418+deedy5@users.noreply.github.com>
2025-07-09 15:38:38 +0300

    fix(text bing): extract url

--- ddgs/engines/bing.py
+++ ddgs/engines/bing.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import base64
 from time import time
 from typing import Any
+from urllib.parse import parse_qs, urlparse
 
 from lxml import html
 
@@ -49,6 +51,12 @@ class Bing(BaseSearchEngine):
             for key, value in self.elements_xpath.items():
                 data = item.xpath(value)
                 data = "".join(x for x in data if x.strip())
+                if key == "href" and data.startswith("https://www.bing.com/ck/a?"):
+                    data = (
+                        lambda u: base64.urlsafe_b64decode((b := u[2:]) + "=" * ((-len(b)) % 4)).decode()
+                        if u and len(u) > 2
+                        else None
+                    )(parse_qs(urlparse(data).query).get("u", [""])[0])
                 result.__setattr__(key, data)
             results.append(result.__dict__)
         return results
