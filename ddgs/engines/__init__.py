df49612c53005369e34068d638f99756f4e83b0f
deedy5 <65482418+deedy5@users.noreply.github.com>
2025-07-13 00:35:59 +0300

    feat(engines): add Yandex

--- ddgs/engines/__init__.py
+++ ddgs/engines/__init__.py
@@ -7,11 +7,13 @@ from .duckduckgo_images import DuckduckgoImages
 from .duckduckgo_news import DuckduckgoNews
 from .duckduckgo_videos import DuckduckgoVideos
 from .google import Google
+from .yandex import Yandex
 
 ENGINES: dict[str, dict[str, type[BaseSearchEngine]]] = {
     "text": {
         "google": Google,
         "bing": Bing,
+        "yandex": Yandex,
         "duckduckgo": Duckduckgo,
     },
     "images": {
