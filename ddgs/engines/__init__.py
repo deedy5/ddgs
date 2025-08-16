125285e20ae5a2428f473646c7dc87e2f4d3fada
deedy5 <65482418+deedy5@users.noreply.github.com>
2025-08-17 02:30:50 +0300

    chore: ruff format

--- ddgs/engines/__init__.py
+++ ddgs/engines/__init__.py
@@ -91,9 +91,7 @@ for finder, modname, _ispkg in pkgutil.iter_modules(package.__path__, package_na
         category = getattr(cls, "category", None)
         if not isinstance(name, str) or not isinstance(category, str):
             msg = f"{cls.__module__}.{cls.__qualname__} must define class attributes 'name: str' and 'category: str'."
-            raise RuntimeError(
-                msg
-            )
+            raise RuntimeError(msg)
 
         ENGINES[category][name] = cls
 
