9f22819fab6f9583cf6426b7126c13f145eb760c
deedy5 <65482418+deedy5@users.noreply.github.com>
2025-08-17 02:20:47 +0300

    chore(lint): add RUFF "DTZ" (flake8-datetimez) rules

--- ddgs/cli.py
+++ ddgs/cli.py
@@ -6,7 +6,7 @@ import csv
 import logging
 import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from datetime import datetime
+from datetime import datetime, timezone
 from pathlib import Path
 from typing import Any
 from urllib.parse import unquote
@@ -48,7 +48,7 @@ def _convert_tuple_to_csv(ctx: click.Context, param: click.Parameter, value: Any
 
 def _save_data(query: str, data: list[dict[str, str]], function_name: str, filename: str | None) -> None:
     filename, ext = filename.rsplit(".", 1) if filename and filename.endswith((".csv", ".json")) else (None, filename)
-    filename = filename if filename else f"{function_name}_{query}_{datetime.now():%Y%m%d_%H%M%S}"
+    filename = filename if filename else f"{function_name}_{query}_{datetime.now(tz=timezone.utc):%Y%m%d_%H%M%S}"
     if ext == "csv":
         _save_csv(f"{filename}.{ext}", data)
     elif ext == "json":
@@ -120,7 +120,7 @@ def _download_results(
     verify: bool = True,
     pathname: str | None = None,
 ) -> None:
-    path = pathname if pathname else f"{function_name}_{query}_{datetime.now():%Y%m%d_%H%M%S}"
+    path = pathname if pathname else f"{function_name}_{query}_{datetime.now(tz=timezone.utc):%Y%m%d_%H%M%S}"
     os.makedirs(path, exist_ok=True)
 
     threads = 10 if threads is None else threads
